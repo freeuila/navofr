bd620f43d6118823c7f0bcb155ffee15c6df6993
nvm <you@example.com>
2020-12-24 23:27:36 +0700

    k

---
+++ configs/directs/vidoza.js
@@ -0,0 +1,171 @@
+
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Vidoza = function () {
+    function Vidoza(props) {
+        _classCallCheck(this, Vidoza);
+
+        this.libs = props.libs;
+        this.settings = props.settings;
+        this.state = {};
+    }
+
+    _createClass(Vidoza, [{
+        key: 'getQuality',
+        value: function getQuality(url) {
+            var qualities = ['DVDRip', 'HDTV', 'HDRip', 'WEB-DL', 'WEBRip', 'BRRip', 'Blu ray', 'Bluray', 'Blu-ray', 'BDRip', 'WEB', 'HDTS', 'TS', 'CAM'];
+
+            for (var i in qualities) {
+                var quality = qualities[i];
+                if (url.toLowerCase().indexOf(quality.toLowerCase()) != -1) {
+                    return quality;
+                }
+            }
+
+            return false;
+        }
+    }, {
+        key: 'checkLive',
+        value: function () {
+            var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(url) {
+                var httpRequest, html;
+                return regeneratorRuntime.wrap(function _callee$(_context) {
+                    while (1) {
+                        switch (_context.prev = _context.next) {
+                            case 0:
+                                httpRequest = this.libs.httpRequest;
+                                _context.prev = 1;
+                                _context.next = 4;
+                                return httpRequest.getHTML(url, { 'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/79.0. 3945.79 Chrome/79.0.3945.79 Safari/537.36' });
+
+                            case 4:
+                                html = _context.sent;
+                                return _context.abrupt('return', html);
+
+                            case 8:
+                                _context.prev = 8;
+                                _context.t0 = _context['catch'](1);
+
+                            case 10:
+                            case 'end':
+                                return _context.stop();
+                        }
+                    }
+                }, _callee, this, [[1, 8]]);
+            }));
+
+            function checkLive(_x) {
+                return _ref.apply(this, arguments);
+            }
+
+            return checkLive;
+        }()
+    }, {
+        key: 'getLink',
+        value: function () {
+            var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2(url) {
+                var _libs, httpRequest, cheerio, sources, html, m, isDie, $, title, quality, s;
+
+                return regeneratorRuntime.wrap(function _callee2$(_context2) {
+                    while (1) {
+                        switch (_context2.prev = _context2.next) {
+                            case 0:
+                                _libs = this.libs, httpRequest = _libs.httpRequest, cheerio = _libs.cheerio;
+
+                                if (!(url.search('https://') == -1 && url.search('http://') == -1)) {
+                                    _context2.next = 3;
+                                    break;
+                                }
+
+                                throw new Error("LINK DIE");
+
+                            case 3:
+                                sources = [];
+                                _context2.next = 6;
+                                return this.checkLive(url);
+
+                            case 6:
+                                html = _context2.sent;
+
+                                if (!(html == false)) {
+                                    _context2.next = 9;
+                                    break;
+                                }
+
+                                throw new Error("LINK DIE");
+
+                            case 9:
+                                m = html.match(/source src="([^"]+)/);
+
+                                if (!(m != undefined)) {
+                                    _context2.next = 23;
+                                    break;
+                                }
+
+                                if (!(m[1].search('https://') != -1 || m[1].search('http://') != -1)) {
+                                    _context2.next = 23;
+                                    break;
+                                }
+
+                                if (!(m[1].indexOf('640x360_universal_july16.mp4') != -1)) {
+                                    _context2.next = 14;
+                                    break;
+                                }
+
+                                throw new Error('FUCKING Vidoza');
+
+                            case 14:
+                                _context2.next = 16;
+                                return httpRequest.isLinkDie(m[1]);
+
+                            case 16:
+                                isDie = _context2.sent;
+                                $ = cheerio.load(html);
+                                title = $('title').text();
+                                quality = this.getQuality(title);
+                                s = {
+                                    label: "NOR",
+                                    file: m[1],
+                                    type: "direct",
+                                    size: isDie ? isDie : ""
+                                };
+
+                                if (quality) s.source_label = quality;
+                                sources.push(s);
+
+                            case 23:
+                                return _context2.abrupt('return', {
+                                    host: {
+                                        url: url,
+                                        name: "vidoza"
+                                    },
+                                    result: sources
+                                });
+
+                            case 24:
+                            case 'end':
+                                return _context2.stop();
+                        }
+                    }
+                }, _callee2, this);
+            }));
+
+            function getLink(_x2) {
+                return _ref2.apply(this, arguments);
+            }
+
+            return getLink;
+        }()
+    }]);
+
+    return Vidoza;
+}();
+
+thisSource.function = function (libs, settings) {
+    return new Vidoza({ libs: libs, settings: settings });
+};
